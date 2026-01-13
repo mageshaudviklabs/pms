75077ba668c75ec3851a4a4e6ac3bd2576bd245b
Rishi-AudvikLabs <rishi.raj@audviklabs.com>
2026-01-13 18:48:18 +0530

    test

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}   
+    return {"message": "Hello World, this is from the backend"}   
 
 
 if __name__ == "__main__":
